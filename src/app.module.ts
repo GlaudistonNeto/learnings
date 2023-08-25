0ac066662c991c39793cc736172947e364220add
glaudistontheonly <glaudistonson@gmail.com>
2023-08-25 16:47:30 -0300

    Error on nestjs/typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [CustomersModule, UsersModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'test',
+    entities: [],
+    synchronize: true
+  })],
   controllers: [],
   providers: [],
 })
