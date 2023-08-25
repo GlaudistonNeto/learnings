64d6496b69f0ae65a9d34a1c675397b23377230c
glaudistontheonly <glaudistonson@gmail.com>
2023-08-24 23:44:40 -0300

    ✨ added exception filters

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -6,18 +6,22 @@ import { SerializedUser, User } from 'src/users/types/index';
 export class UsersService {
   private users: User[] = [
     {
+      id: 0,
       username: 'firstUser',
       password: 'F1password'
     },
     {
+      id: 1,
       username: 'secondUser',
       password: 'S1password'
     },
     {
+      id: 2,
       username: 'thirdUser',
       password: 'T1password'
     },
     {
+      id: 3,
       username: 'fourthUser',
       password: 'F2password'
     },
@@ -29,5 +33,9 @@ export class UsersService {
 
   getUserByUsername(username: string) {
     return this.users.find((user) => user.username === username);
+  }
+
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
   }
 }
